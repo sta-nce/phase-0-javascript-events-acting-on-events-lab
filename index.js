470fb955636ee8a42ad0876df0f59202888ce055
Stanslous <stancemuindi21@gmail.com>
2022-09-05 09:16:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -18,25 +18,14 @@ function moveDodgerRight() {
      dodger.style.left = `${left + 1}px`;
    }
 };
-/*function moveDodgerRight() {
-    const myNumbers = dodger.style.left.replace("px","");
-    const left = parseInt(myNumbers, 10);
-
-    if (left < 0) {
-        dodger.style.left = `${left + 1}`;
-    }
-};*/
 
 document.addEventListener("keydown", function (e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
   };
-});
-
-document.addEventListener("keydown", function (e) {
   if (e.key === "ArrowRight") {
     moveDodgerRight();
    };
 });
-  
+
 
